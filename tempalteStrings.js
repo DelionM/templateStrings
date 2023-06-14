5f3e98c6e53426d3ea2094e775062631707aab8b
Brayan Giovanni Delion Martínez <103006766+DelionM@users.noreply.github.com>
2023-06-13 21:56:22 -0600

    Add files via upload

---
+++ tempalteStrings.js
@@ -0,0 +1,18 @@
+/* Permite trabajar con variables dentro de cadenas de texto */
+
+const nombre = 'Brayan Giovanni';
+const edad = 21;
+const pais = 'México';
+//manejo común para enviar un texto con su variable en consola
+
+console.log('Soy ' + nombre, ' tengo ' + edad, ' años de edad y soy de ' + pais);
+
+/*con el signo `` que se encuentra en mi teclado a lado izquierdo de
+la tecla enter y lo pongo con alt + tecla izquierda del enter
+esto me permite manejar mi código más limpio y es más entendible
+ten cuidado y no te confundas con php 😶
+*/
+console.log(`Soy ${nombre} tengo ${edad} años de edad y soy de ${pais}`);
+
+
+//como vez la salida en consola es igual 😉
